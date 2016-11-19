8e0b21f46db9647aa0aedc9bfc2cefe20d8f123e
Filipe Fernandes <filipeferibeiro@outlook.com>
2016-11-19 00:28:48 -0300

    Adicionado o primeiro tipo de contador.

---
+++ Contador_Tipo_1.sv
@@ -0,0 +1,34 @@
+module Contador_Tipo_1(input logic clock, reset, output logic [3:0] saida);
+	logic posicao;
+	
+	always @(posedge clock)
+		begin
+			if (reset)
+				begin
+					saida <= 4'd0;
+					posicao <= 1'b0;
+				end
+			else
+				begin						
+					if (posicao == 1'b0)
+						if (saida == 4'd15)
+							begin
+								posicao <= 1'b1;
+								saida <= saida - 4'd1;
+							end
+						else
+							saida <= saida + 4'd1;
+							
+							
+					else if (posicao == 1'b1)
+						if (saida == 4'd0)
+							begin
+								posicao <= 1'b0;
+								saida = saida + 1'd1;
+							end
+						else
+							saida <= saida - 4'd1;
+				end
+			end
+
+endmodule
